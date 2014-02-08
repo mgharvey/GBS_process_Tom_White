02849e7f56182c341753075dc8dbe85566b31181
Michael G. Harvey <mharve9@lsu.edu>
2014-02-08 09:43:32 -0600

    Add new version
    Keeping old version (used in Harvey et al. in review) in folder "v1".

---
+++ v2/fasFilter.pl
@@ -0,0 +1,45 @@
+use warnings;
+
+my $infile = $ARGV[0];
+
+my $filename = $ARGV[1];
+open(OUTFILE, ">$filename");
+
+my $filename1 = $ARGV[2];
+open(OUTFILE1, ">$filename1");
+
+
+open(IN,$infile); # or die "ERROR, no input file\n";
+
+
+my @dataArray = ();
+$i = 0;
+
+while($entry=<IN>) {
+	chomp($entry);
+	$entry =~ s/TGCA+\Z/TGCA/g;
+	$dataArray[$i] = $entry;
+	$i++;
+}
+
+print OUTFILE "QUERY\tHIT\n";
+for($j = 0; $j < scalar @dataArray; $j++) {
+
+	if(($j) % 2 == 0) {
+		if(($j) % 4 == 0) {
+			print OUTFILE1 "$dataArray[$j]\n";
+		} else {
+			#print OUTFILE1 "$dataArray[$j]\n";
+		}
+	}
+	if(($j+1) % 2 == 0) {
+		print OUTFILE "$dataArray[$j]";
+		if(($j+1) % 4 == 0) {
+			print OUTFILE "\n";
+		} else {
+			print OUTFILE1 "$dataArray[$j]\n";
+			print OUTFILE "\t";
+		}
+	} 
+	
+}
